3dda7bc2a3dc120f6d86a4c417b2a585c07b469c
Mike Wilcox <mwilcox56@gmail.com>
2015-12-05 19:33:35 -0500

    working build

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
+var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: './build',
-    filename: 'app.js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
   module: {
     loaders: [
       {
-        loader: 'elm-simple-loader',
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      {
+        loaders: ['elm-simple-loader'],
         test: /\.elm$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        include: __dirname
       }
     ]
   }
